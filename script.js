a52267ddd50be00d6b0e7e61637eb0941211b611
edenkibret2010 <edenkibret2010@gmail.com>
2023-07-23 06:03:44 -0200

    css changes

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let taskList = [];
+const taskList = [];
 
 function addTask() {
   const taskInput = document.getElementById("taskInput");
@@ -20,47 +20,16 @@ function addTask() {
   taskList.push(newTask);
   taskInput.value = "";
   reminderInput.value = "";
-  saveTaskListToLocalStorage();
   displayTasks();
 }
 
 function toggleComplete(index) {
   taskList[index].complete = !taskList[index].complete;
-  saveTaskListToLocalStorage();
-  displayTasks();
-}
-
-function editTask(index) {
-  taskList[index].editing = true;
-  displayTasks();
-}
-
-function saveTask(index) {
-  const taskInput = document.getElementById(`taskInput-${index}`);
-  const reminderInput = document.getElementById(`reminderInput-${index}`);
-  const task = taskInput.value.trim();
-  const reminder = reminderInput.value.trim();
-
-  if (task === "") {
-    alert("Please enter a task.");
-    return;
-  }
-
-  taskList[index].task = task;
-  taskList[index].reminder = reminder;
-  taskList[index].editing = false;
-  saveTaskListToLocalStorage();
-  displayTasks();
-}
-
-function cancelEdit(index) {
-  taskList[index].editing = false;
   displayTasks();
 }
 
 function removeTask(index) {
   taskList.splice(index, 1);
-  saveTaskListToLocalStorage();
   displayTasks();
 }
 
@@ -87,7 +56,6 @@ function displayTasks(tasks = taskList) {
   tasks.forEach((task, index) => {
     const listItem = document.createElement("li");
     listItem.className = task.complete ? "complete" : "";
-    listItem.className += task.editing ? " editing" : "";
 
     const taskText = document.createElement("span");
     taskText.innerText = task.task;
@@ -100,21 +68,6 @@ function displayTasks(tasks = taskList) {
     checkbox.checked = task.complete;
     checkbox.onchange = () => toggleComplete(index);
 
-    const editBtn = document.createElement("button");
-    editBtn.className = "editBtn";
-    editBtn.innerText = "Edit";
-    editBtn.onclick = () => editTask(index);
-
-    const saveBtn = document.createElement("button");
-    saveBtn.className = "saveBtn";
-    saveBtn.innerText = "Save";
-    saveBtn.onclick = () => saveTask(index);
-
-    const cancelBtn = document.createElement("button");
-    cancelBtn.className = "cancelBtn";
-    cancelBtn.innerText = "Cancel";
-    cancelBtn.onclick = () => cancelEdit(index);
-
     const removeBtn = document.createElement("button");
     removeBtn.innerText = "Remove";
     removeBtn.onclick = () => removeTask(index);
@@ -122,41 +75,10 @@ function displayTasks(tasks = taskList) {
     listItem.appendChild(taskText);
     listItem.appendChild(reminderText);
     listItem.appendChild(checkbox);
-
-    if (task.editing) {
-      const taskInput = document.createElement("input");
-      taskInput.type = "text";
-      taskInput.id = `taskInput-${index}`;
-      taskInput.value = task.task;
-
-      const reminderInput = document.createElement("input");
-      reminderInput.type = "text";
-      reminderInput.id = `reminderInput-${index}`;
-      reminderInput.value = task.reminder;
-
-      listItem.appendChild(taskInput);
-      listItem.appendChild(reminderInput);
-      listItem.appendChild(saveBtn);
-      listItem.appendChild(cancelBtn);
-    } else {
-      listItem.appendChild(editBtn);
-      listItem.appendChild(removeBtn);
-    }
+    listItem.appendChild(removeBtn);
 
     taskListElement.appendChild(listItem);
   });
 }
 
-function loadTaskListFromLocalStorage() {
-  const savedTaskList = localStorage.getItem("taskList");
-  if (savedTaskList) {
-    taskList = JSON.parse(savedTaskList);
-  }
-}
-
-function saveTaskListToLocalStorage() {
-  localStorage.setItem("taskList", JSON.stringify(taskList));
-}
-
-loadTaskListFromLocalStorage();
 displayTasks();
